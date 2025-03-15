81f1f4db85dc82a27518f3908cd2245fd2d39cc8
Deepak Kumar Yadav <112198305+yadavdeepakk@users.noreply.github.com>
2025-03-15 15:49:05 +0530

    Updated coordinators phone number

--- src/components/Coordinators.jsx
+++ src/components/Coordinators.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import "../styles/Coordinators.css";
 
 const coordinatorsData = [
-  { id: 1, event: "THINK-A-TON", coordinators: ["Shweta Nayal", "Ginni", "Shudhanshu"], phone: ["9876543210", "8765432109", "7654321098"] },
-  { id: 2, event: "BGMI", coordinators: ["Chandan", "Deepak", "Pranchi Gautam"], phone: ["9876501234", "8765409876", "7654312345"] },
-  { id: 3, event: "ECHOES IN PIXELS", coordinators: ["Satyam", "Asma", "Mohit"], phone: ["9988776655", "8877665544", "7766554433"] },
-  { id: 4, event: "QUIZORA", coordinators: ["Pranchi Gautam", "Swastika", "Ashish"], phone: ["9865321470", "8754312098", "7643210987"] },
-  { id: 5, event: "BUISNESS QUIZ", coordinators: ["Farman", "Namrita", "Bhumika"], phone: ["9856741230", "8743219876", "7632109874"] },
-  { id: 6, event: "LOGOVATION", coordinators: ["Mohit Rawat", "Manya", "Archita"], phone: ["9856423178", "8745230981", "7632154890"] },
-  { id: 7, event: "WEB CRAZE", coordinators: ["Deepak", "Vinay", "Mahima"], phone: ["9845123789", "8732541987", "7623410986"] },
-  { id: 8, event: "SPELLBOUND", coordinators: ["Uday", "Devanshi", "Asma"], phone: ["9832419876", "8723541098", "7614320985"] }
+  { id: 1, event: "THINK-A-TON", coordinators: ["Shweta Nayal", "Ginni", "Shudhanshu"], phone: ["9013290305", "9667063972", "8368454457"] },
+  { id: 2, event: "BGMI", coordinators: ["Chandan", "Deepak", "Pranchi Gautam"], phone: ["9810307402", "8081214710", "9560286541"] },
+  { id: 3, event: "ECHOES IN PIXELS", coordinators: ["Satyam", "Asma", "Mohit Rawat"], phone: ["9771111876", "9899638477", "7291007673"] },
+  { id: 4, event: "QUIZORA", coordinators: ["Pranchi Gautam", "Swastika", "Ashish"], phone: ["9560286541", "8383061974", "8851045870"] },
+  { id: 5, event: "BUISNESS QUIZ", coordinators: ["Shivansh", "Namrita", "Bhumika"], phone: ["7703860323", "9650934330", "9990108066"] },
+  { id: 6, event: "LOGOVATION", coordinators: ["Mohit Rawat", "Manya", "Archita"], phone: ["7291007673", "9990009880", "9910222184"] },
+  { id: 7, event: "WEB CRAZE", coordinators: ["Deepak", "Vinay", "Mahima"], phone: ["8081214710", "8700447828", "9266713969"] },
+  { id: 8, event: "SPELLBOUND", coordinators: ["Uday", "Devanshi", "Asma"], phone: ["9971072434", "8789772787", "9899638477"] }
 ];
 
 function Coordinators() {
